9533ee55cb30393d99f0c6f7c651a428e49b94b9
Qwh <qwh005007@gmail.com>
2013-08-10 16:37:08 +0800

    add some arguements

--- main.c
+++ main.c
@@ -3,5 +3,8 @@
 
 int main(int ac, char *av[])
 {
+  // initial
+  queue *qPlayer1, *qPlayer2;    // two player
+  stack *sShit, *sContainor;     // the result and the temp containor to contain it
   return 0;
 }
