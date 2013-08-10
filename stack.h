0c67ddf1c2809aed74e5360fc766cc6191ece035
Qwh <qwh005007@gmail.com>
2013-08-10 12:03:01 +0800

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,7 +1,31 @@
-
-
 #define MAX_LEN_STACK 14
 
-struct stack{
+struct stack
+{
     int content[MAX_LEN_STACK];
+    int top;        // index to top of the stack, point to a valid value
+};
+
+/* push a value into a stack*/
+
+int stack_push(struct *pStack, int value)
+{
+    top++;
+    pStack->content[pStack->top] = value;
+    return 0;
+}
+
+/* pop the top value of the stack*/
+int stack_pop(struct *pStack)
+{
+    if pStack->top == -1
+    {
+        return 1;
+    }
+    else
+    {
+        int iTemp = pStack -> top;
+        top--;
+        return iTemp;
+    }
 }
