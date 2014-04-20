81b6f8104baf67ea529fcd42fa46aa25c20b30d7
Qiwihui <qwh005007@gmail.com>
2014-04-20 22:45:05 +0800

    Update queue.h: change random seed to generate different order at each time.

--- array.h
+++ array.h
@@ -1,22 +1,24 @@
 /* ARRAY.H */
 
-#include<stdlib.h>
+#include <stdlib.h>
+#include <time.h>
 
-/* Arrange the N elements of ARRAY in random order.
-   Only effective if N is much smaller than RAND_MAX;
-   if this may not be the case, use a better random
-   number generator. */
-void shuffle(int *array, size_t n)
+/* Arrange the N elements of ARRAY in random order. */
+void array_shuffle(int *array, size_t n)
 {
     if (n > 1) 
     {
+        // Seed that will get different result
+        srand( time(NULL) );
+        
         size_t i;
-        for (i = 0; i < n - 1; i++) 
+        for (i = n-1; i > 0; i--) 
         {
-          size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
-          int t = array[j];
-          array[j] = array[i];
-          array[i] = t;
+          size_t j = rand() % (i+1);
+          // swap
+          array[i] = array[i] ^ array[j];
+          array[j] = array[i] ^ array[j];
+          array[i] = array[i] ^ array[j];
         }
     }
 }
